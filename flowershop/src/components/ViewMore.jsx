8451d2f1ab09e76af2237e13d31f9e6600eb2f3d
Gopika T G <gopikatg379@gmail.com>
2025-02-19 12:47:55 +0530

    delete functionallity for flower is added

--- flowershop/src/components/ViewMore.jsx
+++ flowershop/src/components/ViewMore.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import Navbar from './Navbar'
 import '../assets/css/ViewMore.css'
 import Footer from './Footer'
@@ -8,6 +8,8 @@ import { Link } from 'react-router-dom'
 const ViewMore = () => {
     const {id} = useParams()
     const [data,setData] = useState({})
+    const[user,setUser]=useState({})
+    const navigate = useNavigate()
     const fetchData = async()=>{
         try{
             const token = localStorage.getItem("access_token");
@@ -16,13 +18,43 @@ const ViewMore = () => {
                 Authorization: `Bearer ${token}`
             }
             })
+            console.log(response.data)
             setData(response.data)
         }catch(error){
             console.log("There was an error",error)
         }
     }
+    const fetchUser = async()=>{
+      try{
+          const token = localStorage.getItem("access_token");
+          const response = await axios.get("http://localhost:8080/user/profile",{
+              headers: {
+                  Authorization: `Bearer ${token}`,
+              },
+          })
+          console.log(response.data)
+          setUser(response.data)
+          
+      }catch(error){
+          console.log("There was an error",error)
+      }
+  }
+  const handleDelete=async()=>{
+    try{
+      const token = localStorage.getItem('access_token')
+      const response = await axios.delete(`http://localhost:8080/flowerShop/delete/${id}`,{
+        headers: {
+          Authorization: `Bearer ${token}`,
+        }
+      })
+      navigate('/home')
+    }catch(error){
+      console.log("There was an error",error)
+    }
+  }
     useEffect(()=>{
-        fetchData()
+        fetchData(),
+        fetchUser()
     },[])
   return (
     <>
@@ -43,12 +75,21 @@ const ViewMore = () => {
         <h6 className="card-subtitle">{data.description}</h6>
         <p className="card-text">{data.price}/-</p>
         <div className="icon-container">
-        <Link to={`/cart/${data.flower_id}`} className="icon-link">
-          <i className="fas fa-shopping-cart" title="Add to Cart"></i>
-        </Link>
-        <Link to={`/wishlist/${data.flower_id}`} className="icon-link">
-          <i className="fas fa-heart wishlist-icon" title="Add to Wishlist"></i>
-        </Link>
+        {user.role === 'USER'?(
+          <div>
+            <Link to={`/cart/${data.flower_id}`} className="icon-link">
+              <i className="fas fa-shopping-cart" title="Add to Cart"></i>
+            </Link>
+            <Link to={`/wishlist/${data.flower_id}`} className="icon-link"> 
+              <i className="fas fa-heart wishlist-icon" title="Add to Wishlist"></i>
+            </Link>
+          </div>
+        ):(
+          <div>
+            <button className='delete-btn' onClick={handleDelete}>Delete</button>
+            <button className='update-btn'>Update</button>
+          </div>
+        )}
       </div>
       </div>
     </div>
